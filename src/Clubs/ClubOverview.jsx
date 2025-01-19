fba52b5b88d1cf7250f3bf01d09916125424935d
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2025-01-19 20:35:01 +0100

    added faviocn to the tab

--- src/Clubs/ClubOverview.jsx
+++ src/Clubs/ClubOverview.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import AllClubs from "./allClubs";
+import { Helmet } from "react-helmet";
 
 function ClubOverview() {
   return (
     <div>
+        <Helmet>
+        <title>Holidaze | Home</title>
+      </Helmet>
       <h1>Club Overview</h1>
       <h2>This page will display an overview of all clubs.</h2>
       <AllClubs /> {/* AllClubs is rendered here */}
