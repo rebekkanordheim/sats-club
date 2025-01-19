4d1e2126926c18365163b404bf349ef03ce4732c
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2025-01-19 16:51:35 +0100

    saving after restoring

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+import Logo from "../images/SatsLogo.jpg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <img src={Logo} alt="SATS Clubs Logo" className="logo-image" />
+        </Link>
+      </div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cluboverview">Club Overview</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
---
+++ src/Layout/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>SATS</h1>
+      <h2>Making people happier and healthier</h2>
+    </div>
+  );
+}
+
+export default Home;
