fa48c329bec6e7331f3350bafa4bc4b94dba0607
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2025-01-19 20:41:10 +0100

    saving

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Helmet } from "react-helmet";
 
 function Home() {
   return (
     <div>
-      <Helmet>
-        <title>SATS | Home</title>
-        <meta name="description" content="Welcome to SATS" />
-      </Helmet>
       <h1>SATS</h1>
       <h2>Making people happier and healthier</h2>
     </div>
