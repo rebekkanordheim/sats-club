0a42c971c6e181b68e275e7782586f78492fd629
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2025-01-19 19:43:25 +0100

    saving before bigger changes

--- src/Clubs/ClubOverview.jsx
+++ src/Clubs/ClubOverview.jsx
@@ -5,7 +5,7 @@ function ClubOverview() {
   return (
     <div>
       <h1>Club Overview</h1>
-      <p>This page will display an overview of all clubs.</p>
+      <h2>This page will display an overview of all clubs.</h2>
       <AllClubs /> {/* AllClubs is rendered here */}
     </div>
   );
